fb580292e534652974ab41af2801ed8e8e7e714e
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-10-24 22:18:12 +0900

    ADD::State store

--- src/store/TableSearchStore.ts
+++ src/store/TableSearchStore.ts
@@ -1,4 +1,42 @@
 import { create } from 'zustand'
+import dayjs, { Dayjs } from 'dayjs'
 
-const useStore = create((set) => ({
+
+interface TableSearchState {
+  searchDate: Dayjs
+  searchItem: string
+  searchWord: string
+  searchDetailItem: string
+  searchDetailWord: string
+
+  setSearchDate: (date: Dayjs) => void
+
+  setSearchItem: (item: string) => void
+  
+  setSearchWord: (word: string) => void
+  
+  setSearchDetailItem: (detailItem: string) => void
+  
+  setSearchDetailWord: (detailWord: string) => void
+}
+
+
+const TableSearchStore = create<TableSearchState>(set => ({
+  searchDate: dayjs(),
+  searchItem: '',
+  searchWord: '',
+  searchDetailItem: '',
+  searchDetailWord: '',
+
+  setSearchDate: date => set(state => ({ searchDate: date })),
+  
+  setSearchItem: item => set(state => ({ searchItem: item })),
+  
+  setSearchWord: word => set(state => ({ searchWord: word })),
+  
+  setSearchDetailItem: detailItem => set(state => ({ searchDetailItem: detailItem })),
+  
+  setSearchDetailWord: detailWord => set(state => ({ searchDetailWord: detailWord })),
 }))
+
+export default TableSearchStore
