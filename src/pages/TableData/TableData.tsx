421e6ad7121dd9ea4bb174ec77988f3956eb1eaf
Ajrdn <ajrdn1276@gmail.com>
2023-10-24 16:28:25 +0900

    ADD::TableDataBody Component

--- src/pages/TableData/TableData.tsx
+++ src/pages/TableData/TableData.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
+import Table from '@mui/material/Table'
 import Box from '@mui/material/Box'
+import TableHeader from './TableDataHeader'
+import TableDataBody from './TableDataBody'
 
 
 const TableData = () => {
   return (
     <TableContainer component={Box}>
-      <Table>
-        
+      <Table sx={{
+        border: '1px solid #C8C8C8',
+        borderRadius: '4px',
+      }}>
+        <TableHeader />
+        <TableDataBody />
       </Table>
     </TableContainer>
   )
