e0f267a61e38ede83738aeedcd0941a285c977f8
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-10-25 21:30:17 +0900

    ADD::TableAddFab

---
+++ src/pages/TableAddFab/TableAddFab.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Fab from '@mui/material/Fab'
+import Add from '@mui/icons-material/Add'
+
+
+const TableAddFab = () => {
+  return (
+    <Fab sx={{
+      bgcolor: '#13243A',
+      position: 'absolute',
+      right: '30px',
+      bottom: '200px',
+      boxShadow: '0px 5px 6px #00000061'
+    }}>
+      <Add sx={{
+        color: 'white',
+      }}/>
+    </Fab>
+  )
+}
+
+export default TableAddFab
