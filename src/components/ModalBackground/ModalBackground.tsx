a8af916b0236ea80a6ff997a57b356d488951cfa
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-10-25 22:06:30 +0900

    ADD::ModalBackground

---
+++ src/components/ModalBackground/ModalBackground.tsx
@@ -0,0 +1,12 @@
+import { styled } from '@mui/material/styles'
+import Modal from '@mui/material/Modal'
+
+
+const ModalBackground = styled(Modal)({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+})
+
+
+export default ModalBackground
