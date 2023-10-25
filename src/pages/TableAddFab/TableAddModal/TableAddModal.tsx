3262b8274f3bbd310b87b56dd407baacd1507047
Ajrdn <ajrdn1276@gmail.com>
2023-10-26 08:33:16 +0900

    UPDATE::Code Clear

--- src/pages/TableAddFab/TableAddModal/TableAddModal.tsx
+++ src/pages/TableAddFab/TableAddModal/TableAddModal.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import InformDataStore from 'store/InformDataStore'
 import ModalBackground from 'components/ModalBackground/ModalBackground'
+import Box from '@mui/material/Box'
 
 
 const TableAddModal = () => {
+  const open = InformDataStore(state => state.open)
+  const setOpen = InformDataStore(state => state.setOpen)
+
+
   return (
-    <ModalBackground>
+    <ModalBackground
+      open={open}
+      onClose={setOpen}
+    >
       <Box>
         
       </Box>
