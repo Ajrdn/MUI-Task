02ae3608c948a49e938e080abb31c9c1a8f2ead2
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-10-25 22:10:43 +0900

    UPDATE::import AlarmModal

--- src/components/AlarmFab/AlarmFab.tsx
+++ src/components/AlarmFab/AlarmFab.tsx
@@ -1,21 +1,28 @@
 import React from 'react'
 import Fab from '@mui/material/Fab'
 import Alarm from '@mui/icons-material/Alarm'
-
+import AlarmModal from './AlarmModal/AlarmModal'
 
 const AlarmFab = () => {
   return (
-    <Fab sx={{
-      bgcolor: 'red',
-      position: 'absolute',
-      right: '30px',
-      bottom: '10px',
-      boxShadow: '0px 5px 6px #00000061'
-    }}>
-      <Alarm sx={{
-        color: 'white',
-      }}/>
-    </Fab>
+    <>
+      <Fab
+        sx={{
+          bgcolor: 'red',
+          position: 'absolute',
+          right: '30px',
+          bottom: '10px',
+          boxShadow: '0px 5px 6px #00000061',
+        }}
+      >
+        <Alarm
+          sx={{
+            color: 'white',
+          }}
+        />
+      </Fab>
+      <AlarmModal />
+    </>
   )
 }
 
