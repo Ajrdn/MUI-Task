acd576d7a8c090cbee4d161c1127ee336521f832
Ajrdn <ajrdn1276@gmail.com>
2023-10-25 16:13:45 +0900

    UPDATE::Keep

--- src/pages/TablePageSearchBar/TablePageSearchBar.tsx
+++ src/pages/TablePageSearchBar/TablePageSearchBar.tsx
@@ -23,34 +23,40 @@ const TablePageSearchBarBackground = styled(Box)({
 
 
 const TablePageSearchBar = () => {
-  const [excelFile, setExcelFile] = useState<any[]>()
+  const [excelFile, setExcelFile] = useState()
 
-  const item = TableSearchStore(state => state.searchItem)
-  const setItem = TableSearchStore(state => state.setSearchItem)
-  const word = TableSearchStore(state => state.searchWord)
-  const setWord = TableSearchStore(state => state.setSearchWord)
+  const item = TableSearchStore((state) => state.searchItem)
+  const setItem = TableSearchStore((state) => state.setSearchItem)
+  const word = TableSearchStore((state) => state.searchWord)
+  const setWord = TableSearchStore((state) => state.setSearchWord)
 
-  const detailItem = TableSearchStore(state => state.searchDetailItem)
-  const setDetailItem = TableSearchStore(state => state.setSearchDetailItem)
-  const detailWord = TableSearchStore(state => state.searchDetailWord)
-  const setDetailWord = TableSearchStore(state => state.setSearchDetailWord)
+  const detailItem = TableSearchStore((state) => state.searchDetailItem)
+  const setDetailItem = TableSearchStore((state) => state.setSearchDetailItem)
+  const detailWord = TableSearchStore((state) => state.searchDetailWord)
+  const setDetailWord = TableSearchStore((state) => state.setSearchDetailWord)
 
-  const taskDataTitleList: string[] = TaskDataListStore(state => state.taskDataTitleList)
-  const taskDataList: TaskData[] = TaskDataListStore(state => state.taskDataList)
+  const taskDataTitleList: string[] = TaskDataListStore(
+    (state) => state.taskDataTitleList
+  )
+  const taskDataList: TaskData[] = TaskDataListStore(
+    (state) => state.taskDataList
+  )
 
-  const setTaskDataListByExcel = TaskDataListStore(state => state.setTaskDataListByExcel)
-  
+  const setTaskDataListByExcel = TaskDataListStore(
+    (state) => state.setTaskDataListByExcel
+  )
+
   const excelDownload = () => {
     TaskDataListDownloadXlsx(taskDataTitleList, taskDataList)
   }
 
-  const excelUpload = useCallback(async (event: React.ChangeEvent<HTMLInputElement>) => {
-    if(!event.target.files) return
+  const excelUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) return
 
     const fileReader = new FileReader()
     fileReader.readAsArrayBuffer(event.target.files[0])
-    fileReader.onload = async (event: ProgressEvent<FileReader>) => {
-      if(!event.target) return
+    fileReader.onload = (event: ProgressEvent<FileReader>) => {
+      if (!event.target) return
       const bufferArray = event.target.result
       const fileInformation = read(bufferArray, {
         type: 'buffer',
@@ -59,28 +65,23 @@ const TablePageSearchBar = () => {
       })
       const sheetName = fileInformation.SheetNames[0]
       const rawData = fileInformation.Sheets[sheetName]
-      const data = utils.sheet_to_json(rawData)
-      setExcelFile(data)
+      const data = utils.sheet_to_json(rawData) as any[]
+      console.log(data)
+      setExcelFile(data[0])
     }
-  }, [])
+  }
 
   const excelDataUpdate = () => {
-    console.log(excelFile)
+    setTimeout(() => {
+      console.log(excelFile)
+    }, 100)
   }
 
   return (
     <TablePageSearchBarBackground>
       <ItemDatePicker />
-      <ItemSelectSearch
-        selectLabel='항목'
-        item={item}
-        setItem={setItem}
-      />
-      <ItemTextSearch
-        textLabel='검색'
-        text={word}
-        setText={setWord}
-      />
+      <ItemSelectSearch selectLabel='항목' item={item} setItem={setItem} />
+      <ItemTextSearch textLabel='검색' text={word} setText={setWord} />
       <ItemSelectSearch
         selectLabel='상세 검색 항목'
         item={detailItem}
