14627549e3a16dbfb1e10aaa29ad752c7ec671b9
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-10-25 22:07:49 +0900

    UPDATE::Keep Updating

--- src/components/InformFab/InformModal/InformModal.tsx
+++ src/components/InformFab/InformModal/InformModal.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import InformDataStore from 'store/InformDataStore'
+import ModalBackground from 'components/ModalBackground/ModalBackground'
 
 
 const InformModal = () => {
+  const open = InformDataStore(state => state.open)
+  const setOpen = InformDataStore(state => state.setOpen)
+
   return (
-    <div>InformModal</div>
+    <ModalBackground
+      open={open}
+      onClose={setOpen}
+    >
+      <Box sx={{ bgcolor: 'red' }}>
+        <Typography id='modal-modal-title' variant='h6' component='h2'>
+          Text in a modal
+        </Typography>
+        <Typography id='modal-modal-description' sx={{ mt: 2 }}>
+          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+        </Typography>
+      </Box>
+    </ModalBackground>
   )
 }
 
