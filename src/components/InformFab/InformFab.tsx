acaf835bdcddc6b9096df59f2967b9a23fe3aa83
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-10-25 22:06:04 +0900

    UPDATE::import InformModal

--- src/components/InformFab/InformFab.tsx
+++ src/components/InformFab/InformFab.tsx
@@ -1,21 +1,33 @@
 import React from 'react'
 import Fab from '@mui/material/Fab'
 import Alarm from '@mui/icons-material/Alarm'
+import InformDataStore from 'store/InformDataStore'
+import InformModal from './InformModal/InformModal'
 
 
 const InformFab = () => {
+  const setOpen = InformDataStore(state => state.setOpen)
+
   return (
-    <Fab sx={{
-      bgcolor: 'green',
-      position: 'absolute',
-      right: '30px',
-      bottom: '100px',
-      boxShadow: '0px 5px 6px #00000061'
-    }}>
-      <Alarm sx={{
-        color: 'white',
-      }}/>
-    </Fab>
+    <>
+      <Fab
+        onClick={setOpen}
+        sx={{
+          bgcolor: 'green',
+          position: 'absolute',
+          right: '30px',
+          bottom: '100px',
+          boxShadow: '0px 5px 6px #00000061',
+        }}
+      >
+        <Alarm
+          sx={{
+            color: 'white',
+          }}
+        />
+      </Fab>
+      <InformModal />
+    </>
   )
 }
 
