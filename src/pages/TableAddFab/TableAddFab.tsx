428fa5f092a69f0226c12eada0ef1679ca0fd14b
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-10-25 22:08:20 +0900

    UPDATE::import TableAddModal

--- src/pages/TableAddFab/TableAddFab.tsx
+++ src/pages/TableAddFab/TableAddFab.tsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import Fab from '@mui/material/Fab'
 import Add from '@mui/icons-material/Add'
+import TableAddModal from './TableAddModal/TableAddModal'
 
 
 const TableAddFab = () => {
   return (
-    <Fab sx={{
-      bgcolor: '#13243A',
-      position: 'absolute',
-      right: '30px',
-      bottom: '200px',
-      boxShadow: '0px 5px 6px #00000061'
-    }}>
-      <Add sx={{
-        color: 'white',
-      }}/>
-    </Fab>
+    <>
+      <Fab
+        sx={{
+          bgcolor: '#13243A',
+          position: 'absolute',
+          right: '30px',
+          bottom: '200px',
+          boxShadow: '0px 5px 6px #00000061',
+        }}
+      >
+        <Add
+          sx={{
+            color: 'white',
+          }}
+        />
+      </Fab>
+      <TableAddModal />
+    </>
   )
 }
 
