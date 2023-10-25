36aad3c7310352a254c179ef7e19b6d58d7c49db
Ajrdn <ajrdn1276@gmail.com>
2023-10-25 10:09:19 +0900

    UPDATE::Zustand Store

--- src/pages/TablePageSearchBar/ItemTextSearch.tsx
+++ src/pages/TablePageSearchBar/ItemTextSearch.tsx
@@ -1,17 +1,16 @@
-import React, { useState } from 'react'
 import TextField from '@mui/material/TextField'
 
 
 interface ItemTextSearchProps {
   textLabel: string
+  text: string
+  setText: (text: string) => void
 }
 
 
 const ItemTextSearch = (props: ItemTextSearchProps) => {
-  const [text, setText] = useState<string>('')
-
   const textChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setText(event.target.value)
+    props.setText(event.target.value)
   }
 
   return (
@@ -20,7 +19,7 @@ const ItemTextSearch = (props: ItemTextSearchProps) => {
       label={props.textLabel}
       placeholder='검색어 입력'
       variant='outlined'
-      value={text}
+      value={props.text}
       onChange={textChange}
       sx={{
         width: '330px',
