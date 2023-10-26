43237adcf519aa1bb578492f870d6bf3775f7c8f
Ajrdn <ajrdn1276@gmail.com>
2023-10-26 13:22:37 +0900

    ADD::TitleField

---
+++ src/components/InformFab/InformModal/TitleField/TitleField.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import TextField from '@mui/material/TextField'
+
+
+const TitleField = () => {
+  return (
+    <TextField
+      placeholder='제목 입력'
+      sx={{
+        width: '100%',
+        '& .MuiInputBase-root': {
+          height: '51px',
+          padding: '15px 20px',
+          borderRadius: '4px',
+          marginBottom: '10px',
+        },
+        '& input': {
+          padding: 0,
+          fontFamily: 'Pretendard',
+          fontWeight: 600,
+          color: '#C8C8C8',
+        },
+      }}
+    />
+  )
+}
+
+export default TitleField
