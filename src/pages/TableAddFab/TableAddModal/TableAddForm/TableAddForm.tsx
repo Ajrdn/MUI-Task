d0099c518accf42fd7fba1cfb7dc2b218b0b5c8f
Ajrdn <ajrdn1276@gmail.com>
2023-10-26 16:36:30 +0900

    ADD::TableAddForm

---
+++ src/pages/TableAddFab/TableAddModal/TableAddForm/TableAddForm.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { styled } from '@mui/material/styles'
+import FormControl from '@mui/material/FormControl'
+import TableAddTextField from './TableAddTextField'
+
+
+const TableAddFormControl = styled(FormControl)({
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  gap: '45px 39px',
+})
+
+
+function TableAddForm() {
+  return (
+    <TableAddFormControl>
+      <TableAddTextField />
+      <TableAddTextField />
+      <TableAddTextField />
+      <TableAddTextField />
+      <TableAddTextField />
+      <TableAddTextField />
+    </TableAddFormControl>
+  )
+}
+
+export default TableAddForm
