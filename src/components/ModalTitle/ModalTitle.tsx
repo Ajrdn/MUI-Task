e83cdff9f21cf07f5cc1f607e3d81f160da84e6e
Ajrdn <ajrdn1276@gmail.com>
2023-10-26 16:12:10 +0900

    UPDATE::Rename

--- src/components/ModalTitle/ModalTitle.tsx
+++ src/components/ModalTitle/ModalTitle.tsx
@@ -2,7 +2,7 @@ import { styled } from '@mui/material/styles'
 import Typography from '@mui/material/Typography'
 
 
-const InformModalTitle = styled(Typography)({
+const ModalTitle = styled(Typography)({
   color: '#13243A',
   fontSize: '23px',
   fontFamily: 'Pretendard',
@@ -10,4 +10,4 @@ const InformModalTitle = styled(Typography)({
   textAlign: 'center',
 })
 
-export default InformModalTitle
+export default ModalTitle
