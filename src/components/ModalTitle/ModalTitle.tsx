d22eca5381d8315e3070cc107d3f044d2d6f8ca0
Ajrdn <ajrdn1276@gmail.com>
2023-10-26 13:11:28 +0900

    ADD::ModalTitle

---
+++ src/components/ModalTitle/ModalTitle.tsx
@@ -0,0 +1,13 @@
+import { styled } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
+
+
+const InformModalTitle = styled(Typography)({
+  color: '#13243A',
+  fontSize: '23px',
+  fontFamily: 'Pretendard',
+  fontWeight: 700,
+  textAlign: 'center',
+})
+
+export default InformModalTitle
