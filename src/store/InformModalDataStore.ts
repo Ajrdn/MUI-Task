35286d118ba2b36811cbcb4a9d4c5addcb56b3a8
Ajrdn <ajrdn1276@gmail.com>
2023-10-26 14:35:02 +0900

    UPDATE::import Target

--- src/store/InformModalDataStore.ts
+++ src/store/InformModalDataStore.ts
@@ -1,7 +1,5 @@
 import { create } from 'zustand'
-
-
-type Target = '생산관리' | '용해' | '슬라브 절단' | '슬라브 가열' | '가열 압연' | '전체 공정'
+import Target from 'interface/Target'
 
 
 interface InformModalDataState {
