e318234c468b921b11712c695ce87fd82b5dab77
Ajrdn <ajrdn1276@gmail.com>
2023-10-26 09:10:31 +0900

    ADD::ModalContentBox

---
+++ src/components/ModalContentBox/ModalContentBox.tsx
@@ -0,0 +1,9 @@
+import { styled } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+
+
+const ModalContentBox = styled(Box)({
+  borderRadius: '25px',
+})
+
+export default ModalContentBox
