244d768a7ffb09e5ab91ac5a58a240be55f536d5
Ajrdn <ajrdn1276@gmail.com>
2023-10-26 10:20:47 +0900

    UPDATE::Close Button

--- src/components/AlarmFab/AlarmModal/AlarmModal.tsx
+++ src/components/AlarmFab/AlarmModal/AlarmModal.tsx
@@ -1,7 +1,38 @@
 import React from 'react'
 import AlarmModalDataStore from 'store/AlarmModalDataStore'
+import { styled } from '@mui/material/styles'
 import Box from '@mui/material/Box'
+import ErrorOutline from '@mui/icons-material/ErrorOutline'
 import ModalBackground from 'components/ModalBackground/ModalBackground'
+import ModalContentBox from 'components/ModalContentBox/ModalContentBox'
+import ModalCloseButton from 'components/ModalCloseButton/ModalCloseButton'
+import AlarmModalTitle from './AlarmModalText/AlarmModalTitle'
+import AlarmModalContent from './AlarmModalText/AlarmModalContent'
+
+
+const AlarmModalBackground = styled(ModalBackground)({
+  backgroundColor: 'rgba(255, 95, 87, 0.3)',
+})
+
+
+const AlarmModalContentBox = styled(ModalContentBox)({
+  justifyContent: 'center',
+  borderRadius: '19px',
+  backgroundColor: '#FF5F57',
+  width: '500px',
+  height: '221px',
+})
+
+
+const MessageBox = styled(Box)({
+  width: '380px',
+  height: '141px',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  color: 'white',
+})
 
 
 const AlarmModal = () => {
@@ -9,14 +40,31 @@ const AlarmModal = () => {
   const setOpen = AlarmModalDataStore(state => state.setOpen)
 
   return (
-    <ModalBackground
+    <AlarmModalBackground
       open={open}
       onClose={setOpen}
     >
-      <Box>
-
-      </Box>
-    </ModalBackground>
+      <AlarmModalContentBox>
+        <MessageBox>
+          <ErrorOutline sx={{
+            width: '42px',
+            height: '42px',
+          }} />
+          <AlarmModalTitle>
+            작업 중단
+          </AlarmModalTitle>
+          <AlarmModalContent>
+            현재 작업을 중단해주시기 바랍니다.
+          </AlarmModalContent>
+        </MessageBox>
+        <ModalCloseButton
+          size='14px'
+          position='25px'
+          color='white'
+          onClick={setOpen}
+        />
+      </AlarmModalContentBox>
+    </AlarmModalBackground>
   )
 }
 
