57ab255c35eb1e1cd44157aced09472c37b85da2
Ajrdn <ajrdn1276@gmail.com>
2023-10-26 10:05:09 +0900

    AlarmModalContent

---
+++ src/components/AlarmFab/AlarmModal/AlarmModalContent/AlarmModalContent.tsx
@@ -0,0 +1,12 @@
+import { styled } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
+
+
+const AlarmModalContent = styled(Typography)({
+  fontSize: '18px',
+  textAlign: 'center',
+  fontFamily: 'Pretendard',
+  fontWeight: 400,
+})
+
+export default AlarmModalContent
