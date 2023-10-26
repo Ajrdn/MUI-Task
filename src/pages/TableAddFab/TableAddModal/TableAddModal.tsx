5abda8071c761c9bc3c1d589c1fcc37e683c8d02
Ajrdn <ajrdn1276@gmail.com>
2023-10-26 16:37:37 +0900

    UPDATE::import TableAddForm

--- src/pages/TableAddFab/TableAddModal/TableAddModal.tsx
+++ src/pages/TableAddFab/TableAddModal/TableAddModal.tsx
@@ -1,19 +1,53 @@
 import React from 'react'
+import { styled } from '@mui/material/styles'
 import TableAddModalDataStore from 'store/TableAddModalDataStore'
 import ModalBackground from 'components/ModalBackground/ModalBackground'
-import Box from '@mui/material/Box'
+import ModalContentBox from 'components/ModalContentBox/ModalContentBox'
+import ModalTitle from 'components/ModalTitle/ModalTitle'
+import ModalCloseButton from 'components/ModalCloseButton/ModalCloseButton'
+import TableAddForm from './TableAddForm/TableAddForm'
 
 
-const TableAddModal = () => {
+const TableAddModalContentBox = styled(ModalContentBox)({
+  borderRadius: '25px',
+  backgroundColor: 'white',
+  width: '800px',
+  height: '592px',
+  padding: '60px 50px 70px',
+})
+
+
+const TableAddModalTitle = styled(ModalTitle)({
+  marginBottom: '45px',
+})
+
+
+function TableAddModal() {
   const open = TableAddModalDataStore(state => state.open)
   const setOpen = TableAddModalDataStore(state => state.setOpen)
+
+  const setClose = () => {
+    setOpen()
+  }
 
   return (
     <ModalBackground
       open={open}
-      onClose={setOpen}
+      onClose={setClose}
     >
-      <Box></Box>
+      <TableAddModalContentBox>
+        <TableAddModalTitle>
+          용해절단 작업일지 추가
+        </TableAddModalTitle>
+        <TableAddForm />
+        <ModalCloseButton
+          size='30px'
+          position='10px'
+          color='#878787'
+          backgroundColor='#F2F2F2'
+          onClick={setClose}
+        />
+      </TableAddModalContentBox>
     </ModalBackground>
   )
 }
