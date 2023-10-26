6b5238893152f638b25916d479cbf636075fa6ae
Ajrdn <ajrdn1276@gmail.com>
2023-10-26 09:00:46 +0900

    UPDATE::Modal State

--- src/components/AlarmFab/AlarmModal/AlarmModal.tsx
+++ src/components/AlarmFab/AlarmModal/AlarmModal.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import AlarmModalDataStore from 'store/AlarmModalDataStore'
+import Box from '@mui/material/Box'
 import ModalBackground from 'components/ModalBackground/ModalBackground'
 
 
 const AlarmModal = () => {
-  return (
-    <ModalBackground>
+  const open = AlarmModalDataStore(state => state.open)
+  const setOpen = AlarmModalDataStore(state => state.setOpen)
 
+  return (
+    <ModalBackground
+      open={open}
+      onClose={setOpen}
+    >
+      <Box>
+
+      </Box>
     </ModalBackground>
   )
 }
--- src/pages/TableAddFab/TableAddModal/TableAddModal.tsx
+++ src/pages/TableAddFab/TableAddModal/TableAddModal.tsx
@@ -1,22 +1,19 @@
 import React from 'react'
-import InformDataStore from 'store/InformDataStore'
+import TableAddModalDataStore from 'store/TableAddModalDataStore'
 import ModalBackground from 'components/ModalBackground/ModalBackground'
 import Box from '@mui/material/Box'
 
 
 const TableAddModal = () => {
-  const open = InformDataStore(state => state.open)
-  const setOpen = InformDataStore(state => state.setOpen)
-
+  const open = TableAddModalDataStore(state => state.open)
+  const setOpen = TableAddModalDataStore(state => state.setOpen)
 
   return (
     <ModalBackground
       open={open}
       onClose={setOpen}
     >
-      <Box>
-        
-      </Box>
+      <Box></Box>
     </ModalBackground>
   )
 }
