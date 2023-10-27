e7b83dbd0ef9140dab67e563a37934b5dff31c51
Ajrdn <ajrdn1276@gmail.com>
2023-10-27 14:44:49 +0900

    UPDATE::Type menuValue

--- src/interface/SearchMenu.ts
+++ src/interface/SearchMenu.ts
@@ -1,3 +1,3 @@
 export type MenuTitle = '전체' | 'LOT No.' | '품종' | '규격' | '슬라브 길이' | '중량'
 
-export type MenuValue = 'All' | 'LOTNo' | 'variety' | 'standard' | 'length' | 'weight'
+export type MenuValue = 'lotNo' | 'variety' | 'standard' | 'length' | 'weight'
