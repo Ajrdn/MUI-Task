c6e0df8d81f048755d5b41a4ff52c23fd21ef5a2
Ajrdn <ajrdn1276@gmail.com>
2023-10-30 08:59:47 +0900

    UPDATE::TaskDataListUploadXlsx Function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,13 @@
-import { utils, writeFile } from 'xlsx'
+import dayjs from 'dayjs'
+import { utils, writeFile, read } from 'xlsx'
 import { TaskDataClient } from 'interface/TaskData'
+import ExcelData from 'interface/ExcelData'
 
 
-export const TaskDataListDownloadXlsx = (taskDataTitleList: string[], taskDataList: TaskDataClient[]) => {
+export const TaskDataListDownloadXlsx = (
+  taskDataTitleList: string[],
+  taskDataList: TaskDataClient[]
+) => {
   const data = taskDataList.map((taskData, index) => ({
     [taskDataTitleList[0]]: (index + 1).toString().padStart(2, '0'),
     [taskDataTitleList[1]]: taskData.workDate.format('YYYY-MM-DD'),
@@ -32,4 +37,33 @@ export const TaskDataListDownloadXlsx = (taskDataTitleList: string[], taskDataLi
   const workbook = utils.book_new()
   utils.book_append_sheet(workbook, datas, excelHandler.getSheetName())
   writeFile(workbook, excelHandler.getExcelFileName())
+}
+
+
+export const TaskDataListUploadXlsx = (event: React.ChangeEvent<HTMLInputElement>, setTaskDataListByList: (taskDataList: TaskDataClient[]) => void) => {
+  if (!event.target.files) return
+  
+  const fileReader = new FileReader()
+  fileReader.readAsArrayBuffer(event.target.files[0])
+  fileReader.onload = (event: ProgressEvent<FileReader>) => {
+    if (!event.target) return
+    const bufferArray = event.target.result
+    const fileInformation = read(bufferArray, {
+      type: 'buffer',
+      cellText: false,
+      cellDates: true,
+    })
+    const sheetName = fileInformation.SheetNames[0]
+    const rawData = fileInformation.Sheets[sheetName]
+    const data: ExcelData[] = utils.sheet_to_json<ExcelData>(rawData)
+    const taskDataList: TaskDataClient[] = data.map((taskData) => ({
+      workDate: dayjs(taskData['작업일']),
+      lotNo: taskData['LOT No.'],
+      variety: taskData['품종'],
+      standard: taskData['규격'],
+      length: taskData['슬라브 길이'],
+      weight: taskData['중량'],
+    }))
+    setTaskDataListByList(taskDataList)
+  }
 }
