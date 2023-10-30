dd2c5ff12f92adca445eddbf91202fa791bd0ccd
Ajrdn <ajrdn1276@gmail.com>
2023-10-30 15:35:24 +0900

    UPDATE::Code Refactoring

--- src/pages/TableData/TableDataBody.tsx
+++ src/pages/TableData/TableDataBody.tsx
@@ -1,24 +1,8 @@
 import React from 'react'
-import { styled } from '@mui/material/styles'
 import TaskDataListStore from 'store/TaskDataListStore'
 import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableRow from '@mui/material/TableRow'
 import { TaskDataClient } from 'interface/TaskData'
-
-
-const TableDataCell = styled(TableCell)({
-  color: '#13243A',
-  fontSize: '13px',
-  fontFamily: 'Pretendard',
-  fontWeight: 400,
-  textAlign: 'center',
-})
-
-
-const TableDataLastCell = styled(TableDataCell)({
-  borderBottom: 'none',
-})
+import TableDataRow from './TableDataRow'
 
 
 function TableDataBody() {
@@ -27,29 +11,23 @@ function TableDataBody() {
   return (
     <TableBody>
       {taskDataShowList.map((taskData, index) => {
-        if(index !== taskDataShowList.length - 1) {
-          return (
-            <TableRow key={`${taskData.lotNo}${index}`}>
-              <TableDataCell align='center'>{(index + 1).toString().padStart(2, '0')}</TableDataCell>
-              <TableDataCell align='center'>{taskData.workDate.format('YYYY-MM-DD')}</TableDataCell>
-              <TableDataCell align='center'>{taskData.lotNo}</TableDataCell>
-              <TableDataCell align='center'>{taskData.variety}</TableDataCell>
-              <TableDataCell align='center'>{taskData.standard}</TableDataCell>
-              <TableDataCell align='center'>{taskData.length}</TableDataCell>
-              <TableDataCell align='center'>{taskData.weight}</TableDataCell>
-            </TableRow>
-          )
-        }
         return (
-          <TableRow key={`${taskData.lotNo}${index}`}>
-            <TableDataLastCell align='center'>{(index + 1).toString().padStart(2, '0')}</TableDataLastCell>
-            <TableDataLastCell align='center'>{taskData.workDate.format('YYYY-MM-DD')}</TableDataLastCell>
-            <TableDataLastCell align='center'>{taskData.lotNo}</TableDataLastCell>
-            <TableDataLastCell align='center'>{taskData.variety}</TableDataLastCell>
-            <TableDataLastCell align='center'>{taskData.standard}</TableDataLastCell>
-            <TableDataLastCell align='center'>{taskData.length}</TableDataLastCell>
-            <TableDataLastCell align='center'>{taskData.weight}</TableDataLastCell>
-          </TableRow>
+          <TableDataRow
+            key={`${taskData.lotNo}${index}`}
+            cellDataList={[
+              (index + 1).toString().padStart(2, '0'),
+              taskData.workDate.format('YYYY-MM-DD'),
+              taskData.lotNo,
+              taskData.variety,
+              taskData.standard,
+              taskData.length,
+              taskData.weight,
+            ]}
+            taskDataRow={{
+              index: (index + 1).toString().padStart(2, '0'),
+              ...taskData,
+            }}
+          />
         )
       })}
     </TableBody>
