aef7a831edbf165e2262c449fd4f53ddc511b755
Ajrdn <ajrdn1276@gmail.com>
2023-10-30 11:40:11 +0900

    UPDATE::Default Value

--- src/store/AlarmModalDataStore.ts
+++ src/store/AlarmModalDataStore.ts
@@ -17,7 +17,7 @@ interface AlarmModalDataState {
 const AlarmModalDataStore = create<AlarmModalDataState>(set => ({
   open: false,
   title: '알람 없음',
-  content: '아무런 알람도 오지 않았습니다.',
+  content: '아무 알람도 오지 않았습니다.',
 
   setOpen: () => set(state => ({open: !state.open})),
 
