11ee3ef1c0b35c3df5022c8bb5e87c02db4a7538
Ajrdn <ajrdn1276@gmail.com>
2023-10-30 11:07:07 +0900

    UPDATE::New Const

--- src/interface/TableHeader.ts
+++ src/interface/TableHeader.ts
@@ -1,6 +1,36 @@
-interface TableHeader {
+export interface TableHeader {
   title: string
   size: string
 }
 
-export default TableHeader
+
+export const TABLE_HEADER_LIST: TableHeader[] = [
+  {
+    title: 'No.',
+    size: '64px',
+  },
+  {
+    title: '작업일',
+    size: '128px',
+  },
+  {
+    title: 'LOT No.',
+    size: '300px',
+  },
+  {
+    title: '품종',
+    size: '300px',
+  },
+  {
+    title: '규격',
+    size: '300px',
+  },
+  {
+    title: '슬라브 길이',
+    size: '300px',
+  },
+  {
+    title: '중량',
+    size: '128px',
+  },
+];
