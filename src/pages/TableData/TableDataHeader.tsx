046d4635f0f4fabdd038e11026bf404797b6666e
Ajrdn <ajrdn1276@gmail.com>
2023-10-30 11:10:30 +0900

    UPDATE::import const

--- src/pages/TableData/TableDataHeader.tsx
+++ src/pages/TableData/TableDataHeader.tsx
@@ -1,18 +1,15 @@
 import React from 'react'
-import TaskDataListStore from 'store/TaskDataListStore'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import TableCell from '@mui/material/TableCell'
-import TableHeader from 'interface/TableHeader'
+import { TABLE_HEADER_LIST } from 'interface/TableHeader'
 
 
 const TableDataHeader = () => {
-  const taskDataTitleList: TableHeader[] = TaskDataListStore(state => state.taskDataTitleList)
-
   return (
     <TableHead>
       <TableRow>
-        {taskDataTitleList.map(tableHeader => (
+        {TABLE_HEADER_LIST.map(tableHeader => (
           <TableCell
             key={tableHeader.title}
             align='center'
