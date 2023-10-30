932635d74b618b7a6812fc70c67b725441ab96db
Ajrdn <ajrdn1276@gmail.com>
2023-10-30 11:07:36 +0900

    UPDATE::Search Function Deleted

--- src/store/TaskDataListStore.ts
+++ src/store/TaskDataListStore.ts
@@ -1,73 +1,27 @@
 import { create } from 'zustand'
 import { TaskDataClient } from 'interface/TaskData'
-import TableHeader from 'interface/TableHeader'
-import { MenuValue } from 'interface/SearchMenu'
 
 
 interface TaskDataListState {
-  taskDataTitleList: TableHeader[]
   taskDataDateList: TaskDataClient[]
   taskDataShowList: TaskDataClient[]
 
   setTaskDataDateList: (newTaskDataDateList: TaskDataClient[]) => void
 
-  addTaskDataDateListByObject: (taskData: TaskDataClient) => void
-
-  addTaskDataDateListByList: (taskDataList: TaskDataClient[]) => void
-
   setTaskDataShowList: (newTaskDataDateList: TaskDataClient[]) => void
-
-  setTaskDataShowListBySearchData: (select: MenuValue, word: string, detailSelect?: MenuValue, detailWord?: string) => void
 }
 
 
 const TaskDataListStore = create<TaskDataListState>(set => ({
-  taskDataTitleList: [
-    {
-      title: 'No.',
-      size: '64px',
-    },
-    {
-      title: '작업일',
-      size: '128px',
-    },
-    {
-      title: 'LOT No.',
-      size: '300px',
-    },
-    {
-      title: '품종',
-      size: '300px',
-    },
-    {
-      title: '규격',
-      size: '300px',
-    },
-    {
-      title: '슬라브 길이',
-      size: '300px',
-    },
-    {
-      title: '중량',
-      size: '128px',
-    },
-  ],
   taskDataDateList: [],
   taskDataShowList: [],
 
-  setTaskDataDateList: newTaskDataDateList => set(state => ({taskDataDateList: newTaskDataDateList})),
-
-  addTaskDataDateListByObject: taskData => set(state => ({taskDataDateList: [...state.taskDataDateList, taskData]})),
-
-  addTaskDataDateListByList: taskDataList => set(state => ({taskDataDateList: [...state.taskDataDateList, ...taskDataList]})),
+  setTaskDataDateList: newTaskDataDateList => set(state => ({
+    taskDataDateList: newTaskDataDateList,
+    taskDataShowList: newTaskDataDateList,
+  })),
 
   setTaskDataShowList: newTaskDataDateList => set(state => ({taskDataShowList: newTaskDataDateList})),
-
-  setTaskDataShowListBySearchData: (select, word, detailSelect?, detailWord?) => {
-    set(state => ({taskDataShowList: state.taskDataDateList.filter(taskData => taskData[select].includes(word))}))
-    if(typeof detailSelect !== 'undefined')
-      set(state => ({taskDataShowList: state.taskDataDateList.filter(taskData => taskData[detailSelect].includes(detailWord!))}))
-  }
 }))
 
 export default TaskDataListStore
