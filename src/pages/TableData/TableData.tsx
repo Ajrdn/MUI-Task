ee0a91d34b9b2bfcafe7e40cb4f31a88301c0ac1
Ajrdn <ajrdn1276@gmail.com>
2023-10-31 16:59:29 +0900

    UPDATE::Keep Updating

--- src/pages/TableData/TableData.tsx
+++ src/pages/TableData/TableData.tsx
@@ -1,11 +1,72 @@
+import React, { useState } from 'react'
+import TableDataCopyStore from 'store/TableDataCopyStore'
 import TableContainer from '@mui/material/TableContainer'
 import Table from '@mui/material/Table'
 import Box from '@mui/material/Box'
 import TableHeader from './TableDataHeader'
 import TableDataBody from './TableDataBody'
+import TableSnackbar, { TableSnackbarProps } from 'pages/TableSnackbar/TableSnackbar'
 
 
 function TableData() {
+  const {
+    tableTaskDataRowCopyList,
+    tableTaskDataRowPasteList,
+    clearTableTaskDataRowCopyList,
+    setTableTaskDataRowPasteList,
+  } = TableDataCopyStore()
+
+  const [snackbarData, setSnackbarData] = useState<TableSnackbarProps>({
+    open: false,
+    severity: 'success',
+    message: '',
+    onClose: () => {
+      setSnackbarData({
+        ...snackbarData,
+        open: false,
+      })
+    },
+  })
+
+  const copyData = (event: React.KeyboardEvent) => {
+    if(event.ctrlKey && event.key === 'c') {
+      if(tableTaskDataRowCopyList) {
+        setSnackbarData(preSnackbarData => ({
+          ...preSnackbarData,
+          open: true,
+          severity: 'success',
+          message: '성공적으로 복사되었습니다!',
+        }))
+        setTableTaskDataRowPasteList()
+      } else {
+        setSnackbarData(preSnackbarData => ({
+          ...preSnackbarData,
+          open: true,
+          severity: 'warning',
+          message: '복사할 데이터가 선택되지 않았습니다.',
+        }))
+      }
+    } else if(event.ctrlKey && event.key === 'v') {
+      if(tableTaskDataRowPasteList.length > 0) {
+        
+
+        setSnackbarData(preSnackbarData => ({
+          ...preSnackbarData,
+          open: true,
+          severity: 'success',
+          message: '성공적으로 붙여넣었습니다!',
+        }))
+      } else {
+        setSnackbarData(preSnackbarData => ({
+          ...preSnackbarData,
+          open: true,
+          severity: 'warning',
+          message: '붙여넣기할 데이터가 선택되지 않았습니다.',
+        }))
+      }
+    }
+  }
+
   return (
     <TableContainer
       component={Box}
@@ -13,18 +74,15 @@ function TableData() {
         border: '1px solid #C8C8C8',
         borderRadius: '8px',
       }}
-      onKeyDown={(event: React.KeyboardEvent) => {
-        console.log(event.key)
-      }}
+      onKeyUp={copyData}
       tabIndex={0}
-      onBlur={() => {
-        console.log('포커스 빠짐')
-      }}
+      onBlur={clearTableTaskDataRowCopyList}
     >
       <Table>
         <TableHeader />
         <TableDataBody />
       </Table>
+      <TableSnackbar {...snackbarData} />
     </TableContainer>
   )
 }
