acc69c2795429f9ed18b0036ba01f7d84ea5dde3
Ajrdn <ajrdn1276@gmail.com>
2023-10-31 13:27:43 +0900

    UPDATE::Code Clear

--- src/store/TableDataCopyStore.ts
+++ src/store/TableDataCopyStore.ts
@@ -20,25 +20,16 @@ const TableDataCopyStore = create<TableDataCopyState>((set) => ({
   taskDataRowCopyList: [],
   taskDataRowPasteList: [],
 
-  addTaskDataRowCopyList: (taskDataRow) =>
-    set((state) => ({
-      taskDataRowCopyList: [...state.taskDataRowCopyList, taskDataRow],
-    })),
+  addTaskDataRowCopyList: taskDataRow => set(state => ({taskDataRowCopyList: [...state.taskDataRowCopyList, taskDataRow]})),
 
-  deleteTaskDataRowCopyList: (index) =>
-    set((state) => ({
-      taskDataRowCopyList: state.taskDataRowCopyList.filter(
-        (taskDataRow) => taskDataRow.index !== index
-      ),
-    })),
+  deleteTaskDataRowCopyList: index => set(state => ({taskDataRowCopyList: state.taskDataRowCopyList.filter(taskDataRow => taskDataRow.index !== index)})),
 
-  clearTaskDataRowCopyList: () => set((state) => ({ taskDataRowCopyList: [] })),
+  clearTaskDataRowCopyList: () => set(state => ({taskDataRowCopyList: []})),
 
-  setTaskDataRowPasteList: () =>
-    set((state) => ({
-      taskDataRowPasteList: state.taskDataRowCopyList,
-      taskDataRowCopyList: [],
-    })),
+  setTaskDataRowPasteList: () => set(state => ({
+    taskDataRowPasteList: state.taskDataRowCopyList,
+    taskDataRowCopyList: [],
+  })),
 }))
 
 export default TableDataCopyStore
