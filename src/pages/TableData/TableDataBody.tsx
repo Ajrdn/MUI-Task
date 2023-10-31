003f86fa12a70caa03963281bbff978836ffaf28
Ajrdn <ajrdn1276@gmail.com>
2023-10-31 14:49:12 +0900

    UPDATE::useEffect

--- src/pages/TableData/TableDataBody.tsx
+++ src/pages/TableData/TableDataBody.tsx
@@ -1,32 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import TaskDataListStore from 'store/TaskDataListStore'
+import TableDataCopyStore from 'store/TableDataCopyStore'
 import TableBody from '@mui/material/TableBody'
 import { TaskDataClient } from 'interface/TaskData'
 import TableDataRow from './TableDataRow'
+import TableTaskDataRow from 'interface/TableTaskDataRow'
 
 
 function TableDataBody() {
   const taskDataShowList: TaskDataClient[] = TaskDataListStore(state => state.taskDataShowList)
+  const tableTaskDataRowCopyList: TableTaskDataRow[] = TableDataCopyStore(state => state.tableTaskDataRowCopyList)
+  const addTableTaskDataRowCopyList = TableDataCopyStore(state => state.addTableTaskDataRowCopyList)
+
+  useEffect(() => {
+    taskDataShowList.map((taskData, index) => addTableTaskDataRowCopyList({
+      index,
+      selected: false,
+      taskData: taskData,
+    }))
+  }, [taskDataShowList, addTableTaskDataRowCopyList])
 
   return (
     <TableBody>
-      {taskDataShowList.map((taskData, index) => {
+      {tableTaskDataRowCopyList.map((tableTaskDataRow, index) => {
         return (
           <TableDataRow
-            key={`${taskData.lotNo}${index}`}
+            key={`${tableTaskDataRow.taskData.lotNo}${index}`}
             cellDataList={[
               (index + 1).toString().padStart(2, '0'),
-              taskData.workDate.format('YYYY-MM-DD'),
-              taskData.lotNo,
-              taskData.variety,
-              taskData.standard,
-              taskData.length,
-              taskData.weight,
+              tableTaskDataRow.taskData.workDate.format('YYYY-MM-DD'),
+              tableTaskDataRow.taskData.lotNo,
+              tableTaskDataRow.taskData.variety,
+              tableTaskDataRow.taskData.standard,
+              tableTaskDataRow.taskData.length,
+              tableTaskDataRow.taskData.weight,
             ]}
-            taskDataRow={{
-              index: (index + 1).toString().padStart(2, '0'),
-              ...taskData,
-            }}
+            taskDataRow={tableTaskDataRow}
           />
         )
       })}
