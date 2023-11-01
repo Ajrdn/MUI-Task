a0ffb0c1c3c6f454893a6f96d61106ab104ca44d
Ajrdn <ajrdn1276@gmail.com>
2023-11-01 16:38:21 +0900

    UPDATE::constant deleted

--- src/interface/TableHeader.ts
+++ src/interface/TableHeader.ts
@@ -1,15 +1,7 @@
-export interface TableHeader {
+interface TableHeader {
   title: string
   size: string
   filterFunction?: (filterData: string, filterFunction: (newData: string) => void) => void
 }
 
-export const TABLE_HEADER_LIST: string[] = [
-  'No.',
-  '작업일',
-  'LOT No.',
-  '품종',
-  '규격',
-  '슬라브 길이',
-  '중량',
-];
+export default TableHeader
