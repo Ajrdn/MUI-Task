a95679942fbc2430f1218e5b4fc80f17575d83f2
Ajrdn <ajrdn1276@gmail.com>
2023-11-01 16:00:41 +0900

    UPDATE::Filter API

--- src/store/TaskDataListStore.ts
+++ src/store/TaskDataListStore.ts
@@ -12,6 +12,8 @@ interface TaskDataListState {
   setTaskDataShowList: (newTaskDataDateList: TaskData[]) => void
 
   setTaskDataShowListBySearchData: (select: MenuValue, word: string, detailSelect?: MenuValue, detailWord?: string) => void
+
+  setTaskDataShowListByFilter: (lotNo: string, variety: string, standard: string, length: string, weight: string) => void
 }
 
 
@@ -30,7 +32,15 @@ const TaskDataListStore = create<TaskDataListState>(set => ({
     set(state => ({taskDataShowList: state.taskDataDateList.filter(taskData => taskData[select].includes(word.toUpperCase()) || taskData[select].includes(word.toLowerCase()))}))
     if(detailSelect)
       set(state => ({taskDataShowList: state.taskDataShowList.filter(taskData => taskData[detailSelect].includes(detailWord!.toUpperCase()) || taskData[detailSelect].includes(detailWord!.toLowerCase()))}))
-  }
+  },
+
+  setTaskDataShowListByFilter: (lotNo, variety, standard, length, weight) => set(state => ({taskDataShowList: state.taskDataDateList.filter(taskData =>
+    (taskData.lotNo.includes(lotNo.toUpperCase()) || taskData.lotNo.includes(lotNo.toLowerCase())) &&
+    (taskData.variety.includes(variety.toUpperCase()) || taskData.variety.includes(variety.toLowerCase())) &&
+    (taskData.standard.includes(standard.toUpperCase()) || taskData.standard.includes(standard.toLowerCase())) &&
+    (taskData.length.includes(length.toUpperCase()) || taskData.length.includes(length.toLowerCase())) &&
+    (taskData.weight.includes(lotNo.toUpperCase()) || taskData.weight.includes(weight.toLowerCase()))
+  )})),
 }))
 
 export default TaskDataListStore
