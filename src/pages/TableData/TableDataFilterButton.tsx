6f162ade7a5e149fa501a6329d94885fd7ef9e71
Ajrdn <ajrdn1276@gmail.com>
2023-11-01 16:55:30 +0900

    UPDATE::Modal

--- src/pages/TableData/TableDataFilterButton.tsx
+++ src/pages/TableData/TableDataFilterButton.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from '@mui/material/Modal'
+import Box from '@mui/material/Box'
 import IconButton from '@mui/material/IconButton'
 import FilterAlt from '@mui/icons-material/FilterAlt'
 
 
 function TableDataFilterButton() {
+  const [open, setOpen] = useState<boolean>(false)
+
+  const openChange = () => {
+    setOpen(true)
+  }
+
   return (
-    <IconButton>
+    <IconButton onClick={openChange}>
       <FilterAlt />
+      <Modal
+        open={open}
+        onClose={openChange}
+      >
+        <Box>
+          test
+        </Box>
+      </Modal>
     </IconButton>
   )
 }
