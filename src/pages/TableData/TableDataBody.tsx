00be09e698f1a186044f8be58cfc4332bf216073
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-11-05 20:54:11 +0900

    UPDATE::New Props

--- src/pages/TableData/TableDataBody.tsx
+++ src/pages/TableData/TableDataBody.tsx
@@ -1,47 +1,27 @@
-import React, { useEffect } from 'react'
-import TaskDataListStore from 'store/TaskDataListStore'
-import TableDataCopyStore from 'store/TableDataCopyStore'
+import React from 'react'
 import TableBody from '@mui/material/TableBody'
-import TaskData from 'interface/TaskData'
+import TableRowData from 'interface/TableRowData'
 import TableDataRow from './TableDataRow'
-import TableTaskDataRow from 'interface/TableTaskDataRow'
 
 
-function TableDataBody() {
-  const taskDataShowList: TaskData[] = TaskDataListStore(state => state.taskDataShowList)
-  const tableTaskDataRowCopyList: TableTaskDataRow[] = TableDataCopyStore(state => state.tableTaskDataRowCopyList)
-  const setTableTaskDataRowCopyList = TableDataCopyStore(state => state.setTableTaskDataRowCopyList)
+interface TableDataBodyProps<T> {
+  clickTableRow: (index: number) => void
+  tableDataShowList: TableRowData<T>[]
+}
 
-  useEffect(() => {
-    const tableTaskDataRowList: TableTaskDataRow[] = taskDataShowList.map((taskData, index) => ({
-      index,
-      selected: false,
-      taskData: taskData,
-    }))
 
-    setTableTaskDataRowCopyList(tableTaskDataRowList)
-  }, [taskDataShowList, setTableTaskDataRowCopyList])
-
+function TableDataBody<T>(props: TableDataBodyProps<T>) {
   return (
     <TableBody>
-      {tableTaskDataRowCopyList.map((tableTaskDataRow, index) => {
-        return (
+      {props.tableDataShowList.map(taskDataShow => (
           <TableDataRow
-            key={`${tableTaskDataRow.taskData.lotNo}${index}`}
-            cellDataList={[
-              (index + 1).toString().padStart(2, '0'),
-              tableTaskDataRow.taskData.workDate,
-              tableTaskDataRow.taskData.lotNo,
-              tableTaskDataRow.taskData.variety,
-              tableTaskDataRow.taskData.standard,
-              tableTaskDataRow.taskData.length,
-              tableTaskDataRow.taskData.weight,
-            ]}
-            index={index}
-            selected={tableTaskDataRow.selected}
+            key={`${taskDataShow.index}`}
+            tableRowData={taskDataShow.tableRowData}
+            index={taskDataShow.index}
+            selected={taskDataShow.selected}
+            clickTableRow={props.clickTableRow}
           />
-        )
-      })}
+        ))}
     </TableBody>
   )
 }
