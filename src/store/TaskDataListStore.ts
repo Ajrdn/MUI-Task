01002635042013a7ab047afc1684e4bab633955f
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-11-05 15:38:39 +0900

    UPDATE::New API

--- src/store/TaskDataListStore.ts
+++ src/store/TaskDataListStore.ts
@@ -1,26 +1,41 @@
 import { create } from 'zustand'
 import TaskData from 'interface/TaskData'
+import TableTaskDataRow from 'interface/TableTaskDataRow'
 
 
 interface TaskDataListState {
   taskDataDateList: TaskData[]
-  taskDataShowList: TaskData[]
+  taskDataShowList: TableTaskDataRow[]
+  selectTaskDataShowListLength: number
+  taskDataPasteList: TaskData[]
 
   setTaskDataDateList: (newTaskDataDateList: TaskData[]) => void
 
-  setTaskDataShowList: (newTaskDataDateList: TaskData[]) => void
+  setTaskDataShowList: (newTaskDataDateList: TableTaskDataRow[]) => void
 
   setTaskDataShowListByFilter: (lotNo: string, variety: string, standard: string, length: string, weight: string) => void
+
+  clickTableRow: (index: number) => void
+
+  clearTaskDataShowList: () => void
+
+  setTaskDataPasteList: () => void
 }
 
 
 const TaskDataListStore = create<TaskDataListState>(set => ({
   taskDataDateList: [],
   taskDataShowList: [],
+  selectTaskDataShowListLength: 0,
+  taskDataPasteList: [],
 
   setTaskDataDateList: newTaskDataDateList => set({
     taskDataDateList: newTaskDataDateList,
-    taskDataShowList: newTaskDataDateList,
+    taskDataShowList: newTaskDataDateList.map((taskDataDate, index) => ({
+      index,
+      selected: false,
+      taskData: taskDataDate,
+    })),
   }),
 
   setTaskDataShowList: newTaskDataDateList => set({taskDataShowList: newTaskDataDateList}),
@@ -31,7 +46,37 @@ const TaskDataListStore = create<TaskDataListState>(set => ({
     (taskData.standard.includes(standard.toUpperCase()) || taskData.standard.includes(standard.toLowerCase())) &&
     (taskData.length.includes(length.toUpperCase()) || taskData.length.includes(length.toLowerCase())) &&
     (taskData.weight.includes(lotNo.toUpperCase()) || taskData.weight.includes(weight.toLowerCase()))
-  )})),
+  ).map((taskDataDate, index) => ({
+    index,
+    selected: false,
+    taskData: taskDataDate,
+  }))})),
+
+  clickTableRow: index => set(state => {
+    const newTaskDataShowList = state.taskDataShowList.map(taskDataShow => {
+      if(index === taskDataShow.index) return {
+        ...taskDataShow,
+        selected: !taskDataShow.selected,
+      }
+      return taskDataShow
+    })
+    return {
+      taskDataShowList: newTaskDataShowList,
+      selectTaskDataShowListLength: newTaskDataShowList.filter(taskDataShow => taskDataShow.selected).length
+    }
+  }),
+
+  clearTaskDataShowList: () => set(state => ({taskDataShowList: state.taskDataShowList.map(taskDataShow => ({
+    ...taskDataShow,
+    selected: false,
+  }))})),
+
+  setTaskDataPasteList: () => set(state => {
+    state.clearTaskDataShowList()
+    return {
+      taskDataPasteList: state.taskDataShowList.filter(taskDataShow => taskDataShow.selected).map(taskDataShow => taskDataShow.taskData),
+    }
+  }),
 }))
 
 export default TaskDataListStore
