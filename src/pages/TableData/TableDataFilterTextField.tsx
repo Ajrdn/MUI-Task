86b1ad09d491c9e57d3d6d2e7ae96a01f3648bad
Ajrdn <ajrdn1276@gmail.com>
2023-11-06 16:03:06 +0900

    Design: Add TextField focused border style

--- src/pages/TableData/TableDataFilterTextField.tsx
+++ src/pages/TableData/TableDataFilterTextField.tsx
@@ -1,5 +1,27 @@
 import React from 'react'
+import { styled } from '@mui/material/styles'
 import TextField from '@mui/material/TextField'
+
+
+const FilterTextField = styled(TextField)({
+  '& input': {
+    fontSize: '16px',
+    fontFamily: 'Pretendard',
+    fontWeight: 400,
+    color: '#13243A',
+  },
+  '& input::placeholder': {
+    fontSize: '16px',
+    fontFamily: 'Pretendard',
+    fontWeight: 400,
+    color: '#C8C8C8',
+  },
+  '& label': {
+    fontFamily: 'Pretendard',
+    fontWeight: 600,
+    color: '#878787',
+  },
+})
 
 
 interface TableDataFilterTextFieldProps {
@@ -14,24 +36,12 @@ function TableDataFilterTextField(props: TableDataFilterTextFieldProps) {
   }
 
   return (
-    <TextField
+    <FilterTextField
+      focused
+      label='검색어 입력'
       value={props.filterData}
       onChange={filterDataChange}
       placeholder='검색어 입력'
-      sx={{
-        '& input': {
-          fontSize: '16px',
-          fontFamily: 'Pretendard',
-          fontWeight: 400,
-          color: '#13243A',
-        },
-        '& input::placeholder': {
-          fontSize: '16px',
-          fontFamily: 'Pretendard',
-          fontWeight: 400,
-          color: '#C8C8C8',
-        },
-      }}
     />
   )
 }
