fe3c96756e4da0b9965652c9e547db0e2f710b86
Ajrdn <ajrdn1276@gmail.com>
2023-11-06 10:56:57 +0900

    Style: Rename attribute name of 'TableHeader' interface

--- src/interface/TableHeader.ts
+++ src/interface/TableHeader.ts
@@ -2,7 +2,7 @@ interface TableHeader {
   title: string
   size: string
   filterData?: string
-  filterFunction?: (newFilterData: string) => void
+  setFilterData?: (newFilterData: string) => void
 }
 
 export default TableHeader
