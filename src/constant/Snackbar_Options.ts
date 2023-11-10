03430ccb1fed0867d4c3684d251e2b69beb4dfc3
Ajrdn <ajrdn1276@gmail.com>
2023-11-10 15:07:54 +0900

    Feat: Create Snackbar Model by Snackbar_Options

--- src/constant/Snackbar_Options.ts
+++ src/constant/Snackbar_Options.ts
@@ -1,6 +1,23 @@
-const SNACKBAR_OPTIONS = {
+interface SNACKBAR_OPTIONS {
+  variant? : 'success' | 'warning' | 'default' | 'error' | 'info',
+  autoHideDuration?: number,
+  disableWindowBlurListener?: boolean,
+}
+
+
+const SNACKBAR_DEFAULT_OPTIONS: SNACKBAR_OPTIONS = {
   autoHideDuration: 3000,
   disableWindowBlurListener: true,
 }
 
-export default SNACKBAR_OPTIONS
+
+export const SNACKBAR_SUCCESS: SNACKBAR_OPTIONS = {
+  ...SNACKBAR_DEFAULT_OPTIONS,
+  variant: 'success',
+}
+
+
+export const SNACKBAR_WARNING: SNACKBAR_OPTIONS = {
+  ...SNACKBAR_DEFAULT_OPTIONS,
+  variant: 'warning',
+}
