aa9574504f5edf44432fb1ca6ff21042df74cffc
Ajrdn <ajrdn1276@gmail.com>
2023-11-13 14:41:49 +0900

    Refactor: Refactor filter function

--- src/pages/TableComponent.tsx
+++ src/pages/TableComponent.tsx
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from 'react'
-import dayjs, { Dayjs } from 'dayjs'
+import { Dayjs } from 'dayjs'
 import { styled } from '@mui/material/styles'
 import Box from '@mui/material/Box'
 import TableHeader from 'interface/TableHeader'
-import MeltingTableData from 'interface/MeltingTableData'
 import TableRowData from 'interface/TableRowData'
 import ExcelData from 'interface/ExcelData'
-import MeltingData from 'interface/MeltingData'
 import TableBar from './TableBar/TableBar'
 import TableData from './TableData/TableData'
-import TableAddFab from './TableAddFab/TableAddFab'
+import TableRowStringData from 'interface/TableRowStringData'
 
 
 const TablePageBackground = styled(Box)({
@@ -20,254 +18,87 @@ const TablePageBackground = styled(Box)({
 })
 
 
-function TableComponent() {
-  const [date, setDate] = useState<Dayjs>(dayjs())
-  const [meltingTableDataList, setMeltingTableDataList] = useState<MeltingTableData[]>([])
-  const [meltingTableDataShowList, setMeltingTableDataShowList] = useState<TableRowData<MeltingTableData>[]>([])
+interface TableComponentProps<TableDataType> {
+  date: Dayjs
+  setDate: (date: Dayjs) => void
+  tableHeaderList: TableHeader<TableDataType>[]
+  tableDataList: TableDataType[]
+  excelFunction: (tableDataExcelList: TableDataType[]) => void
+  no?: boolean
+  pasteFunction?: (tableDataPasteList: TableDataType[]) => Promise<void>
+  copyFunction?: (tableData: TableDataType) => Promise<void>
+  modifyFunction?: (tableData: TableDataType) => Promise<void>
+  deleteFunction?: (tableData: TableDataType) => Promise<void>
+}
 
-  const [lotNo, setLotNo] = useState<string>('')
-  const [variety, setVariety] = useState<string>('')
-  const [standard, setStandard] = useState<string>('')
-  const [length, setLength] = useState<string>('')
-  const [weight, setWeight] = useState<string>('')
 
-  const tableHeaderList: TableHeader<MeltingTableData>[] = [
-    {
-      title: '작업일',
-      size: '128px',
-    },
-    {
-      title: 'LOT No.',
-      size: '300px',
-      setFilterData: setLotNo,
-    },
-    {
-      title: '품종',
-      size: '300px',
-      setFilterData: setVariety,
-    },
-    {
-      title: '규격',
-      size: '300px',
-      setFilterData: setStandard,
-    },
-    {
-      title: '슬라브 길이',
-      size: '300px',
-      setFilterData: setLength,
-    },
-    {
-      title: '중량',
-      size: '128px',
-      setFilterData: setWeight,
-    },
-  ]
-
-  const excelData = meltingTableDataShowList.map(tableDataShow => {
-    const meltingExcelData: ExcelData = {}
-    const tableHeaderTitleList = tableHeaderList.map(tableHeader => tableHeader.title)
-    tableHeaderTitleList.map(title => meltingExcelData[title] = tableDataShow.tableData[title])
-    return meltingExcelData
+function TableComponent<TableDataType>(props: TableComponentProps<TableDataType>) {
+  const [tableDataShowList, setTableDataShowList] = useState<TableRowData<TableDataType>[]>([])
+  const tableKeyList = props.tableHeaderList.map(tableHeader => tableHeader.key)
+  const excelData = tableDataShowList.map(tableDataShow => {
+    const tableExcelData: ExcelData = {}
+    for(const key of tableKeyList) {
+      tableExcelData[key as string] = tableDataShow.tableData[key] as string
+    }
+    return tableExcelData
   })
 
   useEffect(() => {
-    fetch(`http://localhost:8000/taskDataList/${date.format('YYYY-MM-DD')}`)
-      .then(response => response.json())
-      .then((newMeltingDataList: MeltingData[]) => {
-        if(lotNo !== '') setLotNo('')
-        if(variety !== '') setVariety('')
-        if(standard !== '') setStandard('')
-        if(length !== '') setLength('')
-        if(weight !== '') setWeight('')
-        meltingDataListConverter(newMeltingDataList)
-      })
-  }, [date])
+    tableDataListConverter(props.tableDataList)
+  }, [props.tableDataList])
 
   useEffect(() => {
-    filterMeltingTableDataShowList(meltingTableDataList)
-  }, [lotNo, variety, standard, length, weight, meltingTableDataList])
+    filterTableDataShowList()
+  }, [...props.tableHeaderList.filter(tableHeader => tableHeader.filterData !== undefined).map(tableHeader => tableHeader.filterData), props.tableDataList])
 
-  const setNewMeltingTableDataList = (url: string, method: string, body: MeltingData | MeltingData[]): Promise<void> => {
-    return fetch(url, {
-      method: method,
-      body: JSON.stringify(body),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    .then(response => response.json())
-    .then((meltingDataList: MeltingData[]) => {
-      meltingDataListConverter(meltingDataList)
-    })
-  }
-
-  const pasteFunction = (meltingDataPasteList: MeltingTableData[]): Promise<void> => {
-    const meltingDataList: MeltingData[] = meltingDataPasteList.map(meltingDataPaste => {
+  const tableDataListConverter = (newTableDataList: TableDataType[]) => {
+    setTableDataShowList(newTableDataList.map((tableData, index) => {
+      const tableRowStringData: TableRowStringData<TableDataType>[] = props.tableHeaderList.map(tableHeader => ({
+        data: tableData[tableHeader.key] as string,
+        key: tableHeader.setFilterData ? tableHeader.key : undefined,
+      }))
       return {
-        id: meltingDataPaste.ID,
-        workDate: date.format('YYYY-MM-DD'),
-        lotNo: meltingDataPaste['LOT No.'],
-        variety: meltingDataPaste.품종,
-        standard: meltingDataPaste.규격,
-        length: meltingDataPaste['슬라브 길이'],
-        weight: meltingDataPaste.중량,
+        index,
+        selected: false,
+        tableData,
+        tableRowStringData,
       }
-    })
-    return setNewMeltingTableDataList(
-      `http://localhost:8000/taskDataList/${date.format('YYYY-MM-DD')}`,
-      'Put',
-      meltingDataList,
-    )
+    }))
   }
 
-  const copyFunction = (meltingDataCopy: MeltingTableData): Promise<void> => {
-    const meltingData: MeltingData = {
-      id: meltingDataCopy.ID,
-      workDate: date.format('YYYY-MM-DD'),
-      lotNo: meltingDataCopy['LOT No.'],
-      variety: meltingDataCopy.품종,
-      standard: meltingDataCopy.규격,
-      length: meltingDataCopy['슬라브 길이'],
-      weight: meltingDataCopy.중량,
-    }
-    return setNewMeltingTableDataList(
-      `http://localhost:8000/taskDataList/${date.format('YYYY-MM-DD')}`,
-      'Post',
-      meltingData,
-    )
-  }
-
-  const modifyFunction = (meltingDataCopy: MeltingTableData): Promise<void> => {
-    const meltingData: MeltingData = {
-      id: meltingDataCopy.ID,
-      workDate: date.format('YYYY-MM-DD'),
-      lotNo: meltingDataCopy['LOT No.'],
-      variety: meltingDataCopy.품종,
-      standard: meltingDataCopy.규격,
-      length: meltingDataCopy['슬라브 길이'],
-      weight: meltingDataCopy.중량,
-    }
-    return setNewMeltingTableDataList(
-      `http://localhost:8000/taskDataList/update/${date.format('YYYY-MM-DD')}`,
-      'Post',
-      meltingData,
-    )
-  }
-
-  const deleteFunction = (meltingDataCopy: MeltingTableData): Promise<void> => {
-    const meltingData: MeltingData = {
-      id: meltingDataCopy.ID,
-      workDate: date.format('YYYY-MM-DD'),
-      lotNo: meltingDataCopy['LOT No.'],
-      variety: meltingDataCopy.품종,
-      standard: meltingDataCopy.규격,
-      length: meltingDataCopy['슬라브 길이'],
-      weight: meltingDataCopy.중량,
-    }
-    return setNewMeltingTableDataList(
-      `http://localhost:8000/taskDataList/${date.format('YYYY-MM-DD')}`,
-      'Delete',
-      meltingData,
-    )
-  }
-
-  const excelFunction = (meltingDataExcelList: MeltingTableData[]): Promise<void> => {
-    const meltingDataList: MeltingData[] = meltingDataExcelList.map(meltingDataExcel => {
-      return {
-        id: meltingDataExcel.ID,
-        workDate: date.format('YYYY-MM-DD'),
-        lotNo: meltingDataExcel['LOT No.'],
-        variety: meltingDataExcel.품종,
-        standard: meltingDataExcel.규격,
-        length: meltingDataExcel['슬라브 길이'],
-        weight: meltingDataExcel.중량,
-      }
-    })
-    return setNewMeltingTableDataList(
-      `http://localhost:8000/taskDataList/${date.format('YYYY-MM-DD')}`,
-      'Put',
-      meltingDataList,
-    )
-  }
-
-  const meltingTableDataListConverter = (newMeltingTableDataList: MeltingTableData[]) => {
-    setMeltingTableDataShowList(newMeltingTableDataList.map((meltingTableDataDate, index) => ({
-      index,
-      selected: false,
-      tableData: meltingTableDataDate,
-      tableRowStringData: [
-        {
-          data: meltingTableDataDate['작업일'],
-        },
-        {
-          data: meltingTableDataDate['LOT No.'],
-          key: 'LOT No.',
-        },
-        {
-          data: meltingTableDataDate['품종'],
-          key: '품종',
-        },
-        {
-          data: meltingTableDataDate['규격'],
-          key: '규격',
-        },
-        {
-          data: meltingTableDataDate['슬라브 길이'],
-          key: '슬라브 길이',
-        },
-        {
-          data: meltingTableDataDate['중량'],
-          key: '중량',
-        },
-      ],
-    })))
-  }
-
-  const meltingDataListConverter = (newMeltingDataList: MeltingData[]) => {
-    setMeltingTableDataList(newMeltingDataList.map(meltingData => ({
-      ID: meltingData.id,
-      작업일: meltingData.workDate,
-      'LOT No.': meltingData.lotNo,
-      품종: meltingData.variety,
-      규격: meltingData.standard,
-      '슬라브 길이': meltingData.length,
-      중량: meltingData.weight,
-    })))
-  }
-
-  const filterMeltingTableDataShowList = (newMeltingTableDataDateList: MeltingTableData[]) => {
-    const newMeltingTableDataShowList = newMeltingTableDataDateList.filter(meltingTableData =>
-      (meltingTableData['LOT No.'].includes(lotNo.toUpperCase()) || meltingTableData['LOT No.'].includes(lotNo.toLowerCase())) &&
-      (meltingTableData['품종'].includes(variety.toUpperCase()) || meltingTableData['품종'].includes(variety.toLowerCase())) &&
-      (meltingTableData['규격'].includes(standard.toUpperCase()) || meltingTableData['규격'].includes(standard.toLowerCase())) &&
-      (meltingTableData['슬라브 길이'].includes(length.toUpperCase()) || meltingTableData['슬라브 길이'].includes(length.toLowerCase())) &&
-      (meltingTableData['중량'].includes(weight.toUpperCase()) || meltingTableData['중량'].includes(weight.toLowerCase())))
-    meltingTableDataListConverter(newMeltingTableDataShowList)
+  const filterTableDataShowList = () => {
+    const filterList = props.tableHeaderList.filter(tableHeader => tableHeader.filterData !== undefined).map(tableHeader => ({
+      key: tableHeader.key,
+      filterData: tableHeader.filterData,
+    }))
+    const newMeltingTableDataShowList = props.tableDataList.filter(tableData =>
+      filterList.map(filter => (tableData[filter.key] as string).includes(filter.filterData!.toUpperCase()) || (tableData[filter.key] as string).includes(filter.filterData!.toLowerCase()))
+      .reduce((filterData, checkFilter) => filterData && checkFilter, true))
+    tableDataListConverter(newMeltingTableDataShowList)
   }
 
   return (
     <>
       <TablePageBackground>
-        <TableBar<MeltingTableData>
-          date={date} // 날짜
-          setDate={setDate} // 날짜 변경 시 실행할 함수
-          tableDataShowListLength={meltingTableDataShowList.length} // 보여지는 테이블 행 수
-          excelFunction={excelFunction} // 액셀 업로드 시 실행할 함수
+        <TableBar<TableDataType>
+          date={props.date} // 날짜
+          setDate={props.setDate} // 날짜 변경 시 실행할 함수
+          tableDataShowListLength={tableDataShowList.length} // 보여지는 테이블 행 수
+          excelFunction={props.excelFunction} // 액셀 업로드 시 실행할 함수
           excelData={excelData} // 액셀에 나타낼 데이터
         />
-        <TableData<MeltingTableData>
-          date={date}
-          tableHeaderList={tableHeaderList} // 테이블 헤더 리스트
-          tableDataShowList={meltingTableDataShowList} // 보여줄 테이블 데이터
-          setTableDataShowList={setMeltingTableDataShowList} // 보여줄 테이블 데이터 변경 함수
-          no // 테이블에 숫자 열 추가 여부
-          pasteFunction={pasteFunction} // 복사(ctrl + c) 후 붙여넣기(ctrl + v) 시 실행할 함수
-          copyFunction={copyFunction} // 복사버튼 클릭 시 실행할 함수
-          modifyFunction={modifyFunction} // 수정 시 실행할 함수
-          deleteFunction={deleteFunction} // 삭제 시 실행할 함수
+        <TableData<TableDataType>
+          date={props.date}
+          tableHeaderList={props.tableHeaderList} // 테이블 헤더 리스트
+          tableDataShowList={tableDataShowList} // 보여줄 테이블 데이터
+          setTableDataShowList={setTableDataShowList} // 보여줄 테이블 데이터 변경 함수
+          no={props.no} // 테이블에 숫자 열 추가 여부
+          pasteFunction={props.pasteFunction} // 복사(ctrl + c) 후 붙여넣기(ctrl + v) 시 실행할 함수
+          copyFunction={props.copyFunction} // 복사버튼 클릭 시 실행할 함수
+          modifyFunction={props.modifyFunction} // 수정 시 실행할 함수
+          deleteFunction={props.deleteFunction} // 삭제 시 실행할 함수
         />
       </TablePageBackground>
-      <TableAddFab setMeltingTableDataList={meltingDataListConverter}/>
     </>
   )
 }
